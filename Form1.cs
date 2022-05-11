02f284761952e07406681facef2cca43a4344b1f
Marin <fantom2413@gmail.com>
2022-05-11 13:36:54 +0500

    Добавил кнопку

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace git_test
         {
             label1.Text = "ВОт и всё!";
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Нажата кнопка";
+        }
     }
 }
